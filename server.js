697efe61ad86a9ef69fa41b7764ae58f0f6c78be
David <arattanasack@gmail.com>
2020-03-12 20:55:35 -0700

    test push

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const express = require("express"),
+ path = require("path"),
+ PORT = process.env.PORT || 3001,
+ app = express(),
+ mongoose = require('mongoose')
+ db = mongoose.connection;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +11,16 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 const root = require('path').join(__dirname, 'client', 'build')
 app.use(express.static(root));
+
+
+// Mongoose
+mongoose.connect("mongodb://user:password1@ds111565.mlab.com:11565/heroku_4m970fc5", { useNewUrlParser: true });
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to db')
+});
+
 
 app.get("*", (req, res) => {
   res.sendFile('index.html', {
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+
 function App() {
   return (
     <div>
-
+      <h1>test</h1>
     </div>
   );
 }
